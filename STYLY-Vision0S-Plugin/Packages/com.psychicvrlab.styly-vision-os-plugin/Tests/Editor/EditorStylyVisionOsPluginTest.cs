e5609034075d2475f7d68afa63d8380fb46c6d55
afjk <afjk01@gmail.com>
2023-11-26 09:54:11 +0900

    Add AssetBundleUtility

--- STYLY-Vision0S-Plugin/Packages/com.psychicvrlab.styly-vision-os-plugin/Tests/Editor/EditorStylyVisionOsPluginTest.cs
+++ STYLY-Vision0S-Plugin/Packages/com.psychicvrlab.styly-vision-os-plugin/Tests/Editor/EditorStylyVisionOsPluginTest.cs
@@ -1,28 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace Styly.VisionOs
+namespace Styly.VisionOs.Plugin
 {
     public class EditorStylyVisionOsPluginTest
     {
         // A Test behaves as an ordinary method
         [Test]
-        public void EditorVisionOsPluginTestSimplePasses()
+        public void SwitchPlatformToVisionOs()
         {
-            // Use the Assert class to test conditions
+            EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneOSX);
+            Assert.That(EditorUserBuildSettings.activeBuildTarget, Is.EqualTo(BuildTarget.StandaloneOSX));
+            
+            var abUtility = new AssetBundleUtility();
+            var result = abUtility.SwitchPlatform(BuildTarget.VisionOS);
+            
+            Assert.That(result, Is.True);
+            Assert.That(EditorUserBuildSettings.activeBuildTarget, Is.EqualTo(BuildTarget.VisionOS));
         }
-
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator EditorVisionOsPluginTestWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
-        }
+        
+        
+        //
+        // // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // // `yield return null;` to skip a frame.
+        // [UnityTest]
+        // public IEnumerator EditorVisionOsPluginTestWithEnumeratorPasses()
+        // {
+        //     // Use the Assert class to test conditions.
+        //     // Use yield to skip a frame.
+        //     yield return null;
+        // }
     }
 }
