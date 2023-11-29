a38b185c869f78f7b25b04a1f74da5c12cce0748
afjk <afjk01@gmail.com>
2023-11-29 21:05:10 +0900

    Create About Popup Window with UI Toolkit

--- STYLY-Vision0S-Plugin/Packages/com.styly.styly-vision-os-plugin/Editor/Menu/AboutPopupWindow.cs
+++ STYLY-Vision0S-Plugin/Packages/com.styly.styly-vision-os-plugin/Editor/Menu/AboutPopupWindow.cs
@@ -1,62 +1,36 @@
-using Styly.VisionOs.Plugin;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class AboutPopupWindow : EditorWindow
+namespace Styly.VisionOs.Plugin
 {
-    private Texture2D logo;
-    private GUIStyle linkStyle;
-
-    [MenuItem("Assets/STYLY/About", false, 90009)]
-    public static void ShowWindow()
+    public class AboutPopupWindow : EditorWindow
     {
-        var window = EditorWindow.GetWindow(typeof(AboutPopupWindow), true, "About STYLY");
-        window.maxSize = new Vector2(600, 200);
-        window.minSize = new Vector2(600, 200);
-    }
+        [SerializeField]
+        private VisualTreeAsset m_VisualTreeAsset = default;
 
-    private void OnEnable()
-    {
-        // Link style
-        linkStyle = new GUIStyle();
-        linkStyle.fontStyle = FontStyle.Italic;
-        linkStyle.hover.textColor = new Color(0.8f, 0.8f, 1f);
-        linkStyle.normal.textColor = EditorGUIUtility.isProSkin ? new Color(0.6f, 0.6f, 1f) : Color.blue;
-
-        // Load STYLY logo
-        string logoPath = $"Packages/{Config.PackageName}/Editor/UI/STYLY.png"; // ロゴの相対パス
-        logo = AssetDatabase.LoadAssetAtPath<Texture2D>(logoPath);
-    }
-
-
-    void OnGUI()
-    {
-        if (logo != null)
+        [MenuItem("Assets/STYLY/About", false, 90009)]
+        public static void ShowAboutStyly()
         {
-            // ロゴの表示
-            GUILayout.Label(logo, GUILayout.Width(1143 / 10), GUILayout.Height(417 / 10));
-        }
-        else
-        {
-            EditorGUILayout.LabelField($"ロゴが読み込めません。パッケージ名が{Config.PackageName}から変更されましたか？");
+            var wnd = GetWindow(typeof(AboutPopupWindow), true, "About STYLY");
+            wnd.titleContent = new GUIContent("About STYLY");
         }
 
-        EditorGUILayout.LabelField("STYLY Vision OS Plugin for Unity");
-        EditorGUILayout.LabelField("Unity Version: " + Application.unityVersion);
-        EditorGUILayout.LabelField("Plugin Version: " + Styly.VisionOs.Plugin.PackageManagerUtility.Instance.GetPackageVersion(Config.PackageName));
-
-        // サイトへのリンク
-        // ハイパーリンクのようなテキストの表示
-        if (GUILayout.Button("Visit Our Website", linkStyle))
+        public void CreateGUI()
         {
-            Application.OpenURL("https://styly.cc/");
-        }
+            // Each editor window contains a root VisualElement object
+            VisualElement root = rootVisualElement;
+        
+            // Instantiate UXML
+            VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
+            root.Add(labelFromUXML);
 
-        // マウスオーバー時のカーソル変更
-        Rect buttonRect = GUILayoutUtility.GetLastRect();
-        if (buttonRect.Contains(Event.current.mousePosition))
-        {
-            EditorGUIUtility.AddCursorRect(buttonRect, MouseCursor.Link);
+            var packageVersion = PackageManagerUtility.Instance.GetPackageVersion(Config.PackageName);
+            var versionLabel = root.Q<Label>("PluginVersionLabel");
+            versionLabel.text = packageVersion;
+
+            var unityVersionLabel = root.Q<Label>("UnityVersionLabel");
+            unityVersionLabel.text = UnityEngine.Application.unityVersion;
         }
     }
 }
