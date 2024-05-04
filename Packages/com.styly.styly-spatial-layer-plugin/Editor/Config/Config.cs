b88130a352390cf925b0f6560c30b4b030941d9e
Masahiro Yamaguchi <from2001@hotmail.com>
2024-05-04 22:23:29 +0900

    Updated config.cs

--- Packages/com.styly.styly-spatial-layer-plugin/Editor/Config/Config.cs
+++ Packages/com.styly.styly-spatial-layer-plugin/Editor/Config/Config.cs
@@ -6,7 +6,7 @@ namespace Styly.VisionOs.Plugin
         public static readonly int ThumbnailHeight = 1024;
         public static readonly string OutputPath = "_Output";
         public static readonly string UploadPage =  "https://spatial-layer.styly.cc/";
-        public static readonly string PackageName = "com.styly.styly-vision-os-plugin";
+        public static readonly string PackageName = "com.styly.styly-spatial-layer-plugin";
         public static readonly string VisualScriptingName = "com.unity.visualscripting";
     }
 }
