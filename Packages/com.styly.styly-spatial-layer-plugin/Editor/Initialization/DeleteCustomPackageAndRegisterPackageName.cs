a21eeabd18ad09402cb76ba5bea524c991836fca
Masahiro Yamaguchi <from2001@hotmail.com>
2024-05-14 14:53:51 +0900

    Refactored PackageManagerUtility code

--- Packages/com.styly.styly-spatial-layer-plugin/Editor/Initialization/DeleteCustomPackageAndRegisterPackageName.cs
+++ Packages/com.styly.styly-spatial-layer-plugin/Editor/Initialization/DeleteCustomPackageAndRegisterPackageName.cs
@@ -5,6 +5,7 @@ using UnityEditor;
 using System.IO;
 using Newtonsoft.Json;
 using System.Linq;
+using Styly.VisionOs.Plugin;
 
 namespace Styly
 {
@@ -33,10 +34,10 @@ namespace Styly
                 if (Directory.Exists(MyPackagePath)) { Directory.Delete(MyPackagePath, true); }
 
                 // Add a scoped registry of the OpenUPM package
-                AddScopedRegistryOfOpenUpmPackage(MyPackageName);
+                PackageManagerUtility.AddScopedRegistryOfOpenUpmPackage(MyPackageName);
 
                 // Add the package to the project
-                AddUnityPackage(MyPackageName + "@" + MyPackageVersion);
+                PackageManagerUtility.AddUnityPackage(MyPackageName + "@" + MyPackageVersion);
             }
         }
 
@@ -62,74 +63,6 @@ namespace Styly
             return false;
         }
 
-        /// <summary>
-        /// Add a Unity package to the project
-        /// </summary>
-        /// <param name="packageName">
-        /// Example: com.company.packaganame or com.company.packaganame@0.1.1
-        /// </param>
-        static void AddUnityPackage(string packageName)
-        {
-            var request = UnityEditor.PackageManager.Client.Add(packageName);
-            while (!request.IsCompleted) { }
-            if (request.Error != null) { Debug.LogError(request.Error.message); }
-        }
 
-        /// <summary>
-        /// Add a scoped registry of the OpenUPM package
-        /// </summary>
-        static void AddScopedRegistryOfOpenUpmPackage(string packageName)
-        {
-            AddScopedRegistry(new ScopedRegistry
-            {
-                name = "package.openupm.com",
-                url = "https://package.openupm.com",
-                scopes = new string[] {
-                packageName
-            }
-            });
-        }
-
-        /// <summary>
-        /// Add a scoped registry to the manifest.json file only if it doesn't already exist.
-        /// </summary>
-        static void AddScopedRegistry(ScopedRegistry pScopeRegistry)
-        {
-            var manifestPath = Path.Combine(Application.dataPath, "..", "Packages/manifest.json");
-            var manifestJson = File.ReadAllText(manifestPath);
-            var manifest = JsonConvert.DeserializeObject<ManifestJson>(manifestJson);
-            var existingRegistry = manifest.scopedRegistries.FirstOrDefault(r => r.name == pScopeRegistry.name);
-
-            if (existingRegistry != null)
-            {
-                // Check if the scope already exists
-                if (!existingRegistry.scopes.Contains(pScopeRegistry.scopes[0]))
-                {
-                    // Add the new scope to the existing registry
-                    var scopesList = existingRegistry.scopes.ToList();
-                    scopesList.Add(pScopeRegistry.scopes[0]);
-                    existingRegistry.scopes = scopesList.ToArray();
-                }
-            }
-            else
-            {
-                // Add the new registry
-                manifest.scopedRegistries.Add(pScopeRegistry);
-            }
-            File.WriteAllText(manifestPath, JsonConvert.SerializeObject(manifest, Formatting.Indented));
-        }
-
-        class ScopedRegistry
-        {
-            public string name;
-            public string url;
-            public string[] scopes;
-        }
-
-        class ManifestJson
-        {
-            public Dictionary<string, string> dependencies = new();
-            public List<ScopedRegistry> scopedRegistries = new();
-        }
     }
 }
--- Packages/com.styly.styly-spatial-layer-plugin/Editor/Utility/PackageManagerUtility.cs
+++ Packages/com.styly.styly-spatial-layer-plugin/Editor/Utility/PackageManagerUtility.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEditor.PackageManager;
 using UnityEngine;
+using Styly;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace Styly.VisionOs.Plugin
 {
@@ -31,5 +34,79 @@ namespace Styly.VisionOs.Plugin
             // Return an empty string or null if the package is not found
             return null;
         }
+    
+        /// <summary>
+        /// Add a Unity package to the project
+        /// </summary>
+        /// <param name="packageName">
+        /// Example: com.company.packaganame or com.company.packaganame@0.1.1
+        /// </param>
+        public static void AddUnityPackage(string packageName)
+        {
+            var request = UnityEditor.PackageManager.Client.Add(packageName);
+            while (!request.IsCompleted) { }
+            if (request.Error != null) { Debug.LogError(request.Error.message); }
+        }
+
+        /// <summary>
+        /// Add a scoped registry of the OpenUPM package
+        /// </summary>
+        public static void AddScopedRegistryOfOpenUpmPackage(string packageName)
+        {
+            AddScopedRegistry(new ScopedRegistry
+            {
+                name = "package.openupm.com",
+                url = "https://package.openupm.com",
+                scopes = new string[] {
+                packageName
+            }
+            });
+        }
+
+        /// <summary>
+        /// Add a scoped registry to the manifest.json file only if it doesn't already exist.
+        /// </summary>
+        static void AddScopedRegistry(ScopedRegistry pScopeRegistry)
+        {
+            var manifestPath = Path.Combine(Application.dataPath, "..", "Packages/manifest.json");
+            var manifestJson = File.ReadAllText(manifestPath);
+            var manifest = JsonConvert.DeserializeObject<ManifestJson>(manifestJson);
+            var existingRegistry = manifest.scopedRegistries.FirstOrDefault(r => r.name == pScopeRegistry.name);
+
+            if (existingRegistry != null)
+            {
+                // Check if the scope already exists
+                if (!existingRegistry.scopes.Contains(pScopeRegistry.scopes[0]))
+                {
+                    // Add the new scope to the existing registry
+                    var scopesList = existingRegistry.scopes.ToList();
+                    scopesList.Add(pScopeRegistry.scopes[0]);
+                    existingRegistry.scopes = scopesList.ToArray();
+                }
+            }
+            else
+            {
+                // Add the new registry
+                manifest.scopedRegistries.Add(pScopeRegistry);
+            }
+            File.WriteAllText(manifestPath, JsonConvert.SerializeObject(manifest, Formatting.Indented));
+        }
+
+        class ScopedRegistry
+        {
+            public string name;
+            public string url;
+            public string[] scopes;
+        }
+
+        class ManifestJson
+        {
+            public Dictionary<string, string> dependencies = new();
+            public List<ScopedRegistry> scopedRegistries = new();
+        }
+    
+    
+    
+    
     }
 }
