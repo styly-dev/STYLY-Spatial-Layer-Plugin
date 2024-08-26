4f8b8cb9a902a7879be76b613c60e633a2b37ccb
afjk <afjk01@gmail.com>
2024-08-26 19:47:24 +0900

    Add SimpleHttpServer and Output AssetBundle to Serve directory

---
+++ Packages/com.styly.styly-spatial-layer-plugin/Editor/SimpleHttpServer/SimpleHttpServerEditor.cs
@@ -0,0 +1,73 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+public class SimpleHttpServerEditor : EditorWindow
+{
+    private string serverUrl = "";
+    private SimpleHttpServer server;
+    private bool serverRunning = false;
+
+    [MenuItem("STYLY/Simple HTTP Server")]
+    public static void ShowWindow()
+    {
+        GetWindow<SimpleHttpServerEditor>("Simple HTTP Server");
+    }
+
+    private void OnEnable()
+    {
+        server = new SimpleHttpServer();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Simple HTTP Server", EditorStyles.boldLabel);
+
+        if (!serverRunning)
+        {
+            if (GUILayout.Button("Start Server"))
+            {
+                server.StartServer();
+                serverRunning = true;
+
+                serverUrl = $"http://{SimpleHttpServer.GetHostName()}:{server.port}{server.path}";
+            }
+        }
+        else
+        {
+            if (GUILayout.Button("Stop Server"))
+            {
+                server.StopServer();
+                serverRunning = false;
+                serverUrl = "";
+            }
+        }
+
+        GUILayout.Space(10);
+
+        if (!string.IsNullOrEmpty(serverUrl))
+        {
+            GUILayout.Label("Server URL:", EditorStyles.boldLabel);
+            EditorGUILayout.TextField(serverUrl);
+
+            if (GUILayout.Button("Copy URL"))
+            {
+                EditorGUIUtility.systemCopyBuffer = serverUrl;
+            }
+
+            if (GUILayout.Button("Open in Browser"))
+            {
+                Application.OpenURL(serverUrl);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (serverRunning)
+        {
+            server.StopServer();
+        }
+    }
+}
+#endif
